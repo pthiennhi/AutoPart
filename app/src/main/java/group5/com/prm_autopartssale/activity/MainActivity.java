b1111ce1f19591b3934a3cf2a909726891b1c99d
MinhPhuc <phucnvm2210@gmail.com>
2023-10-24 20:42:18 +0700

    Setup Source
    Signed-off-by: MinhPhuc <phucnvm2210@gmail.com>

---
+++ app/src/main/java/group5/com/prm_autopartssale/activity/MainActivity.java
@@ -0,0 +1,25 @@
+package group5.com.prm_autopartssale.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import group5.com.prm_autopartssale.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
+
+    }
+}
